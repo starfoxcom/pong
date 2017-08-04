41ebc4c2e4ad3143efaef5e45a4bbf765c4bb93e
Alex Zamudio <idv15c.jzamudio@uartesdigitales.edu.mx>
2017-08-04 16:20:55 -0500

    creacion del proyecto
    se creo el proyecto en unity

---
+++ pong/Assets/scripts/ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+
+
+    private Rigidbody rb;
+    private float speed;
+    private Vector3 dir;
+
+    // Use this for initialization
+	void Start () {
+        speed = 10;
+        rb = GetComponent<Rigidbody>();
+        dir = new Vector3(0f, 0f, 1.0f);
+        dir.Normalize();
+        GetComponent<Rigidbody>().velocity += dir * speed;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
