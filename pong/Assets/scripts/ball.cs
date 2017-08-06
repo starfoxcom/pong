5f619b7827cee28ae67c117ff6a2650f38bcb675
Alex Zamudio <idv15c.jzamudio@uartesdigitales.edu.mx>
2017-08-06 01:05:19 -0500

    topspin
    se crearon las funciones para realizar el topspin

--- pong/Assets/scripts/ball.cs
+++ pong/Assets/scripts/ball.cs
@@ -6,23 +6,18 @@ public class ball : MonoBehaviour {
 
 
     public float speed;
-    private Vector3 diry, diri, pos;
+    private Rigidbody rb;
+    private Vector3 diry, diri, pos, topspin;
 
     // Use this for initialization
 	void Start () {
         
         diri = new Vector3(0f, 0f, Random.Range(-1f,1f));
         pos = GetComponent<Rigidbody>().position;
-        //while (diri.x != 0)
-        //{
-        //    diri.x = Random.Range(-1f, 1f);
-        //}
-      
+        rb = GetComponent<Rigidbody>();
         diry = new Vector3(0f, 0.4f, 0f);
         diri.Normalize();
-        //diry.Normalize();
         GetComponent<Rigidbody>().velocity = diri * speed;
-        //GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -30,20 +25,43 @@ public class ball : MonoBehaviour {
         if(collision.gameObject.name == "p1")
         {
             var p1 = collision.gameObject;
-            GetComponent<Rigidbody>().velocity = ((p1.GetComponent<Rigidbody>().transform.forward + diry) * speed);
-
+            if (Input.GetKey(KeyCode.T))
+            {
+                topspin = rb.velocity;
+                topspin.z *= 1.5f;
+                topspin.y = 4;
+                if (topspin.z > 14.5f)
+                    topspin.z = 14.5f;
+                rb.velocity = topspin;
+            }
+            else
+                rb.velocity = ((p1.GetComponent<Rigidbody>().transform.forward + diry) * speed);
+            //Debug.Log(rb.velocity);
+            //Debug.Log(topspin);
         }
         if (collision.gameObject.name == "p2")
         {
             var p2 = collision.gameObject;
-            GetComponent<Rigidbody>().velocity = -((-p2.GetComponent<Rigidbody>().transform.forward - diry) * speed);
+            if (Input.GetKey(KeyCode.Comma))
+            {
+                topspin = rb.velocity;
+                topspin.z *= 1.5f;
+                topspin.y = 4;
+                if (topspin.z < -14.5f)
+                    topspin.z = -14.5f;
+                rb.velocity = topspin;
+            }
+            else
+                rb.velocity = -((-p2.GetComponent<Rigidbody>().transform.forward - diry) * speed);
+            //Debug.Log(rb.velocity);
+            //Debug.Log(topspin);
         }
         if (collision.gameObject.name == "out")
         {
-            GetComponent<Rigidbody>().position = pos;
+            rb.position = pos;
             diri = new Vector3(0f, 0f, Random.Range(-1f, 1f));
             diri.Normalize();
-            GetComponent<Rigidbody>().velocity = diri * speed;
+            rb.velocity = diri * speed;
             Debug.Log(speed);
         }
 
--- pong/Assets/scripts/player2.cs
+++ pong/Assets/scripts/player2.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class player2 : MonoBehaviour {
-    public float speed;
+    public float speed, slide;
     private Vector3 mov, position;
     private Rigidbody rb;
     float angle;
@@ -13,24 +13,39 @@ public class player2 : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
     }
 
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.name == "ball")
+    //    {
+    //        var ball = collision.gameObject;
+    //        ball.GetComponent<Rigidbody>().velocity += mov * slide;
+    //        Debug.Log(mov);
+    //        Debug.Log(slide);
+    //        Debug.Log(ball.GetComponent<Rigidbody>().velocity);
+    //    }
+    //}
+
     // Update is called once per frame
     void Update()
     {
         rotation = transform.rotation;
         position = transform.position;
         angle = rotation.w;
-        if (Input.GetKey(KeyCode.V))
+        if (Input.GetKey(KeyCode.B))
         {
             mov = new Vector3(-1, 0, 0);
             rb.transform.position += mov * speed * Time.deltaTime;
             //rb.transform.Rotate(Vector3.forward * speed * 5 * Time.deltaTime);
         }
-
-        if (Input.GetKey(KeyCode.B))
+        else if (Input.GetKey(KeyCode.V))
         {
             mov = new Vector3(1, 0, 0);
             rb.transform.position += mov * speed * Time.deltaTime;
             //rb.transform.Rotate(Vector3.back * speed * 5 * Time.deltaTime);
+        }
+        else
+        {
+            mov = new Vector3(0, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.N))
@@ -42,6 +57,7 @@ public class player2 : MonoBehaviour {
         {
             rb.transform.Rotate(Vector3.up * speed * 10 * Time.deltaTime);
         }
+
         if (angle > .30f)
             rb.transform.rotation = Quaternion.Euler(0, 145.1f, 0);
         else if (angle < -.30f)
